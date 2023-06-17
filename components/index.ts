6c87967e5cd7eb6766d6142d023f0e1ff776c584
Jonathan Bangala <jbangala90@gmail.com>
2023-06-17 22:35:12 +0200

    fixing errors

--- components/index.ts
+++ components/index.ts
@@ -1,26 +1,19 @@
 import CarCard from "./CarCard";
-import CarDetails from "./CarDetails";
 import CustomButton from "./CustomButton";
 import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
-import Hero from "./Hero";
-import Navbar from "./Navbar";
-import SearchBar from "./SearchBar";
-import SearchManufacturer from "./SearchManufacturer";
+import NavBar from "./Navbar";
 import ShowMore from "./ShowMore";
-
-
-
+import SearchBar from "./SearchBar";
+import Hero from "./Hero";
 
 export {
   Hero,
   CarCard,
   CustomButton,
   CustomFilter,
-  SearchBar,
   Footer,
-  Navbar,
+  NavBar,
   ShowMore,
-  CarDetails,
-  SearchManufacturer
+  SearchBar,
 };
